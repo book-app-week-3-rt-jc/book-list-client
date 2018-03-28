343784e9496fd8c4f81e120043e5bd9b27829c23
Richard Tae <taerichard@yahoo.com>
2018-03-27 17:15:59 -0700

    site works now, updated fetchall

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -30,17 +30,17 @@ var app = app || {};
       let titleA = a.title.toUpperCase();
       let titleB = b.title.toUpperCase();
       return (titleA < titleB) ? -1 : 1;
-    })
+    });
     Book.all = bookData.map(x => new Book(x));
-  }
+  };
 
   Book.fetchAll = callback => {
-    $.get(`https://rt-jc-booklist.herokuapp.com//api/v1/books`)
+    $.get(`https://rt-jc-booklist.herokuapp.com/api/v1/books`)
       .then(results => {
         Book.loadAll(results);
         callback();
-      }, err => errorCallback());
-  }; 
+      }, err => errorCallback(err));
+  };
 
   module.Book = Book;
 
